395fe0d9a3350a8bf72e281d39613f13ba4019d4
Safar Ethan <ethansafar@gmail.com>
2022-11-25 02:53:45 +0100

    remove ALL errors

--- src/pages/form-page.jsx
+++ src/pages/form-page.jsx
@@ -16,9 +16,19 @@ function FormPage (){
 	};
 
 	const changeHandler = async(event) => {
-		// if (event.target.files[0])
-		// {
-
+		if (event.target.files[0])
+		{
+			const data = new FormData();
+			data.append("file", event.target.files[0], event.target.files[0].name);
+			data.append("isSync", true);
+			const res = await starton.post('/ipfs/file',
+				data, {
+						headers: {
+									"x-api-key": 'sk_live_89c7396e-c994-415e-9127-8bb3f7b8a7d4',
+									"Content-Type": "multipart/form-data"},
+				})
+				console.log(res.data);
+		}
 		// 	let reader = new FileReader();
 		// 	reader.readAsDataURL(event.target.files[0]);
 		// 	reader.onload = () => {
@@ -31,30 +41,18 @@ function FormPage (){
 					// 			console.log(response.data);
 					// 		})
 				// }
-		// }
-
 			
 		// 	console.log(typeof event.target.files[0])
 		// 	// console.log(event.target.files[0]);
 		// 	const file1 = new Buffer.from(JSON.stringify(event.target.files[0]));
 		// 	console.log(file1);
 		// 	// const file1 = await toBase64(event.target.files[0]);
-			const data = new FormData();
-			data.append("file", event.target.files[0], event.target.files[0].name);
-			data.append("isSync", true);
-			const res = await starton.post('/ipfs/file',
-				data, {
-						headers: {
-									"x-api-key": 'sk_live_89c7396e-c994-415e-9127-8bb3f7b8a7d4',
-									"Content-Type": "multipart/form-data"},
-				})
-				console.log(res.data);
 	};
     return (
       <>
         <div>
-          <h1 class="logo-over-white">
-            PAYCONSENT<span class="dot-color">.</span>
+          <h1 className="logo-over-white">
+            PAYCONSENT<span className="dot-color">.</span>
           </h1>
         </div>
         <div className="total-form">
@@ -67,18 +65,18 @@ function FormPage (){
               </div>
               <div className="status-div">
                 <div className="label-status">
-                  <label class="status-title" for="contractStatus">
+                  <label className="status-title">
                     Status
                   </label>
                   <p className="text-[13px]">Seller or Buyer</p>
                 </div>
                 <div className="select-box-div">
                   <select className="select-box">
-                    <option value="0" selected disabled>
+                    <option defaultValue="0" disabled>
                       Status
                     </option>
-                    <option value="1">Buyer</option>
-                    <option value="2">Seller</option>
+                    <option defaultValue="1">Buyer</option>
+                    <option defaultValue="2">Seller</option>
                   </select>
                 </div>
               </div>
@@ -86,7 +84,7 @@ function FormPage (){
             <div className="form-div2">
               <div className="status-div">
                 <div className="label-status">
-                  <label class="status-title" for="contractName">
+                  <label className="status-title">
                     My Wallet ID
                   </label>
                   <p className="text-[13px]">The creator of this contract</p>
@@ -102,7 +100,7 @@ function FormPage (){
               </div>
               <div className="status-div">
                 <div className="label-status">
-                  <label class="status-title" for="contractSubject">
+                  <label className="status-title">
                     Subject
                   </label>
                   <p className="text-[13px]">About the contract</p>
@@ -118,7 +116,7 @@ function FormPage (){
               </div>
               <div className="status-div">
                 <div className="label-status">
-                  <label class="status-title" for="contractWalletAddress">
+                  <label className="status-title">
                     Sending To
                   </label>
                   <p className="text-[13px]">The receiver</p>
@@ -133,12 +131,12 @@ function FormPage (){
               </div>
               <div className="status-div-upload">
                 <div className="label-status">
-                  <label class="status-title">File To Upload</label>
+                  <label className="status-title">File To Upload</label>
                   <p className="text-[13px]">(Quotation, Specifications ...)</p>
                 </div>
                 <div>
-                  <div class="upload-btn-wrapper">
-                    <button class="btn" onClick={handleSubmission}>{urls.length == 2 ? "Files Uploaded":"Upload a file"}</button>
+                  <div className="upload-btn-wrapper">
+                    <button className="btn" onClick={handleSubmission}>{urls.length == 2 ? "Files Uploaded":"Upload a file"}</button>
                     <input 
 					// onClick={starton.post('/ipfs/file',
 					// 	{
@@ -155,7 +153,7 @@ function FormPage (){
               </div>
               <div className="status-div">
                 <div className="label-status">
-                  <label class="status-title" for="contractPrice">
+                  <label className="status-title">
                     Price
                   </label>
                 </div>
--- src/pages/main-page.jsx
+++ src/pages/main-page.jsx
@@ -7,17 +7,17 @@ import "../assets/css/home.css";
 class MainPage extends Component {
   render() {
     return (
-      <div class="w-screen h-screen">
+      <div className="w-screen h-screen">
         <div>
-          <h1 class="logo-over-white">
-            PAYCONSENT<span class="dot-color">.</span>
+          <h1 className="logo-over-white">
+            PAYCONSENT<span className="dot-color">.</span>
           </h1>
         </div>
-        <div class="intro-div">
-          <h2 class="intro-title">
+        <div className="intro-div">
+          <h2 className="intro-title">
             Giving You Solutions <br></br>For Smarter Contracts
           </h2>
-          <quote class="intro-quote">
+          <quote className="intro-quote">
             Are you tired of complex and slow administration ? <br></br>Get your
             first decentralized contract, entirely <br></br>managed with
             blockchain technology
